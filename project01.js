54988c05fe116a0c3419ab67c92924700720d4f3
thesagor <imthesagor@gmail.com>
2019-01-18 14:40:26 +0600

    This is day 5 class

--- project01.js
+++ project01.js
@@ -1,5 +1,5 @@
-//ternary operator niye kotha
-var x=15;
-var aritmat= x%2;
-var result=(aritmat == 0) ? "even numer" : "odd number bejor";
-console.log(result);
+//day-5 loop in javascript
+var i=10;
+while(i-->0){
+    console.log(i);
+}
