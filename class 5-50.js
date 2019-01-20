7f1759d1ed7bcb391f80be9de6756b2d4da5c9b4
thesagor <imthesagor@gmail.com>
2019-01-20 13:27:38 +0600

    Day 5.50 (multiple for loop)

---
+++ class 5-50.js
@@ -0,0 +1,6 @@
+// multiple for loop statement
+var x,y;
+for (x=0,y=100;x<=20,y>=80;x++,y--){
+    console.log(x,y);
+}
+//here two statement running and x is increasing to 20 and y is decresing to 80
